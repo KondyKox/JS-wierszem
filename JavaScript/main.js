697c538bfde81cdd0044552acf7f2bc46e127d62
KondyKox <108412658+KondyKox@users.noreply.github.com>
2022-08-12 22:23:37 +0200

    Add files via upload

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,12 +1,12 @@
 function saveToFile() {
     // Variables
     let interpretacja = document.getElementById('interpretacja').value;
-    let ocena = ['Co to za gówno?', 'Nieżla interpretacja mój bracie w Chrystusie.', 'Napisałbym lepszą.', 'GÓWNO!!!']
+    let ocena = ['Co to za gówno?', 'Nieżla interpretacja mój bracie w Chrystusie.', 'Napisałbym lepszą.', 'GÓWNO!!!', "Weź ty lepiej zostań przy klockach LEGO."];
 
-    let index = Math.floor(Math.random() * (ocena.length - 1));
+    let index = Math.floor(Math.random() * (ocena.length));
 
     // Console outputs and saving to file
-    if (interpretacja != null || interpretacja != " ") {
+    if (interpretacja != null) {
         console.log(ocena[index]);
         console.log("Oto twoje 'arcydzieło':");
         console.log(interpretacja);
@@ -16,7 +16,7 @@ function saveToFile() {
         });
         saveAs(blob, "TwojaInterpretacja.txt");
 
-        alert("ZAPISANO!!!");
+        alert("ZAPISANO!");
     }
 
     else {
@@ -25,7 +25,38 @@ function saveToFile() {
 
 }
 
-function getVotes() {
-    let kondi = document.getElementById("kondi");
-    let mickiewicz = document.getElementById("mickiewicz");
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Variables
+let kondi = document.getElementById("kondi");
+let mickiewicz = document.getElementById("mickiewicz");
+
+let voted = false;
+
+function voteKondi() {   
+    if (voted == false) {
+        // let kondiWygral = document.createElement("img");
+        // kondiWygral.src = "../Grafiki/kondiWygral.png";
+
+        // let mickiewiczPrzegral = document.createElement("img");
+        // mickiewiczPrzegral.src = "../Grafiki/mickiewiczPrzegral.png";
+
+        alert("Kondi 'Mega Koks' wygrał!");
+
+        voted = true;
+    }
+}
+
+function voteMickiewicz() {
+    if (voted == false) {
+        // let kondiPrzegral = document.createElement("img");
+        // kondiPrzegral.src = "../Grafiki/kondiPrzegral.png";
+
+        // let mickiewiczWygral = document.createElement("img");
+        // mickiewiczWygral.src = "../Grafiki/mickiewiczWygral.png";
+
+        alert("Adam 'Chuj, nie poeta' Mickiewicz");
+
+        voted = true;
+    }
 }
